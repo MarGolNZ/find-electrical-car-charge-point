e7fb56241c416d5ac75b8008679265aea3fd5bd6
marina <golovina.marina@gmail.com>
2021-06-27 20:06:38 +1000

    Add usePosition hook

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,43 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Nav from 'react-bootstrap/Nav'
-import GetChargeInfo from "./GetChargeInfo"
+// import GetChargeInfo from "./GetChargeInfo"
 // import SearchChargePoint from './SearchChargePoint'
 import ChargePoinstMap from './ChargePoinstMap'
+import { usePosition } from 'use-position'
 
 const App = () => {
+  const watch = true;
+  const {
+    latitude,
+    longitude,
+    timestamp,
+    accuracy,
+    error,
+  } = usePosition(watch)
+
   return (
     <>
-      <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-
-      <div className="row">
+      <div style={{ width: '100%' }} >
+        <ChargePoinstMap />
+      </div>
+      < div className='row' >
         <div className='col'>
-          <h1>Find Your Charge Point</h1>
+          <code>
+            Test Geolocation Data
+            latitude: {latitude}<br />
+            longitude: {longitude}<br />
+            timestamp: {timestamp}<br />
+            accuracy: {accuracy && `${accuracy}m`}<br />
+            error: {error}
+          </code>
         </div>
-
-      </div>
-      <div className='row'>
-        {/* <div><SearchChargePoint /></div> */}
-        <div className='col'><ChargePoinstMap /></div>
-        <div className='col'><GetChargeInfo /></div>
-      </div>
+      </div >
 
     </>
   )
