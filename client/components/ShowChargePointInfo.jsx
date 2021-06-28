870c11f76a9eed6a167688a413bea88d7be297c0
marina <golovina.marina@gmail.com>
2021-06-28 20:50:59 +1000

    Add nearby charging stations list next to the map

--- client/components/ShowChargePointInfo.jsx
+++ client/components/ShowChargePointInfo.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { getChargeInfo } from '../apiClient.js'
 
 export default function GetChargeInfo(props) {
-    const [chargePoint, setChargePoint] = useState({ AddressInfo: { AddressLine1: '', StateOrProvince: '', Title: '', Town: '' } })
+    const [chargePoint, setChargePoint] = useState([])
 
     useEffect(() => {
         getChargeInfo(props.position)
             .then(position => {
                 console.log(position[0])
-                setChargePoint(position[0])
+                setChargePoint(position)
                 return null
             })
             .catch(e => console.log(e))
@@ -17,18 +17,21 @@ export default function GetChargeInfo(props) {
     return (
         <>
             <div className='row'>
-
                 <div className='col'>
                     <h2> Charging Stations near you:</h2>
-                    <p>Name: {chargePoint.AddressInfo.Title}</p>
-                    <p>State: {chargePoint.AddressInfo.StateOrProvince}</p>
-                    <p>City: {chargePoint.AddressInfo.Town}</p>
-                    <p>Address: {chargePoint.AddressInfo.AddressLine1}</p>
+                    <ul>
+                        {chargePoint.map(chargePointAddress => <li key={chargePointAddress.UUID}>
+                            Name: {chargePointAddress.AddressInfo.Title}
+                            State: {chargePointAddress.AddressInfo.StateOrProvince}
+                            City: {chargePointAddress.AddressInfo.Town}
+                            Address: {chargePointAddress.AddressInfo.AddressLine1}
+                            Comments: {chargePointAddress.AddressInfo.AccessComments}
+                        </li>)}
+                    </ul>
                 </div>
             </div>
-
         </>
     )
-
+    // { AddressInfo: { AddressLine1: '', StateOrProvince: '', Title: '', Town: '' } }
 
 }
