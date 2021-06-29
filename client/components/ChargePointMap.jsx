9aa7fc4edde9ccd92b76f10803e5c7fac38b7323
marina <golovina.marina@gmail.com>
2021-06-29 23:10:03 +1000

    Customize marker icon

--- client/components/ChargePointMap.jsx
+++ client/components/ChargePointMap.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import ShowChargePointInfo from './ShowChargePointInfo'
+import L from 'leaflet'
+import { divIcon } from 'leaflet';
 
 export default function ChargePoinstMap(props) {
     const position = [props.latitude, props.longitude]
+    const position2 = [{ latitude: -27.38, longitude: 153.03 }, { latitude: - 27.25, longitude: 153.10 },]
+
+    const icon = divIcon({
+        html: '<i class="fas fa-plug"></i style="max-width:40px">', iconSize: 40
+    })
 
     return (
         < div className='row' >
@@ -13,10 +19,10 @@ export default function ChargePoinstMap(props) {
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}><Popup>Your current Location</Popup></Marker>
+                    {position2.map(point => <Marker position={[point.latitude, point.longitude]} icon={icon}></Marker>)}
+                    < Marker position={position}><Popup>Your current Location</Popup></Marker>
                 </MapContainer>
             </div>
         </div >
     )
 }
-// }
--- client/components/ShowChargePointInfo.jsx
+++ client/components/ShowChargePointInfo.jsx
@@ -8,13 +8,13 @@ export default function GetChargeInfo(props) {
                     <h2> Charging Stations near you:</h2>
                     <ul>
                         {props.chargePoints.map(chargePointAddress => <li key={chargePointAddress.UUID}>
-                            Name: {chargePointAddress.AddressInfo.Title}
-                            State: {chargePointAddress.AddressInfo.StateOrProvince}
-                            City: {chargePointAddress.AddressInfo.Town}
-                            Address: {chargePointAddress.AddressInfo.AddressLine1}
-                            Comments: {chargePointAddress.AddressInfo.AccessComments}
-                            Latitude: {chargePointAddress.AddressInfo.Latitude}
-                            Longitude: {chargePointAddress.AddressInfo.Longitude}
+                            Name: {chargePointAddress.AddressInfo.Title}<br />
+                            State: {chargePointAddress.AddressInfo.StateOrProvince}<br />
+                            City: {chargePointAddress.AddressInfo.Town}<br />
+                            Address: {chargePointAddress.AddressInfo.AddressLine1}<br />
+                            Comments: {chargePointAddress.AddressInfo.AccessComments}<br />
+                            Latitude: {chargePointAddress.AddressInfo.Latitude}<br />
+                            Longitude: {chargePointAddress.AddressInfo.Longitude}<br />
                         </li>)}
                     </ul>
                 </div>
