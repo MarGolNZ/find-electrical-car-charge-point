9b2ebfc03c4e929596524ae0e7da143acc38fb5d
marina <golovina.marina@gmail.com>
2021-07-25 18:50:56 +1000

    Change marker colour of charge points to green

--- client/components/ChargePointMap.jsx
+++ client/components/ChargePointMap.jsx
@@ -4,10 +4,19 @@ import L from 'leaflet';
 
 export default function ChargePoinstMap(props) {
     const position = [props.latitude, props.longitude]
-    const icon = L.icon({
-        iconUrl: '/icon-1.png',
-        iconSize: [40, 'auto']
-    })
+    // const icon = L.icon({
+    //     iconUrl: 'vectorstock_33740292.png', // <a href="https://www.vectorstock.com/royalty-free-vector/eco-car-icon-for-web-and-mobile-vector-33740292">Vector image by VectorStock / vectorstock</a>
+    //     iconSize: [40, 'auto']
+    // })
+
+    const greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
 
     return (
         <div className='map'>
@@ -16,9 +25,12 @@ export default function ChargePoinstMap(props) {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {props.chargePoints.map(point => <Marker position={[point.AddressInfo.Latitude, point.AddressInfo.Longitude]} icon={icon} key={point.UUID}></Marker>)}
+                {props.chargePoints.map(point => <Marker position={[point.AddressInfo.Latitude, point.AddressInfo.Longitude]} icon={greenIcon} key={point.UUID}></Marker>)}
                 <Marker position={position}><Popup>Your current Location</Popup></Marker>
             </MapContainer>
         </div>
     )
 }
+
+
+//<a href='https://www.freepik.com/vectors/car'>Car vector created by stories - www.freepik.com</a>
