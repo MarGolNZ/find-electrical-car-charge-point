b5b15312755f9a4e4f7fb4395533f6424637308f
marina <golovina.marina@gmail.com>
2021-07-25 16:41:18 +1000

    Update header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div classname='box1'>
-            <h1>Find Electrical Charge point <i className="fas fa-charging-station"></i></h1>
+            <h1>Find Electrical Charge point in Australia<i className="fas fa-charging-station"></i></h1>
         </div>
 
     )
